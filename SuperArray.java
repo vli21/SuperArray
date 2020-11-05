a86f0626839f8379e3c3c7659da33a6f4cfc5c14
vli21 <vli21@stuy.edu>
2020-11-05 18:06:51 -0500

    indexOf works

--- SuperArray.java
+++ SuperArray.java
@@ -107,4 +107,14 @@ public class SuperArray{
     }
     return has;
   }
+  public int indexOf(String s){
+    int here=-1;
+    for (int i=0; i <size; i++){
+      if (data[i].equals(s)){
+        here =i;
+        return here;
+      }
+    }
+    return here;
+  }
 }
