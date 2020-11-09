965c0225e009dd42a711be50591afb58e85abb3a
vli21 <vli21@stuy.edu>
2020-11-09 13:51:41 -0500

    findOverlap works with the example provided

--- Demo.java
+++ Demo.java
@@ -1,5 +1,7 @@
 public class Demo{
-
+/*-------
+Methods
+-------*/
   public static void removeDuplicates(SuperArray s){
       for (int i=0; i <s.size();){
         if (s.indexOf(s.get(i))==i){
@@ -11,6 +13,23 @@ public class Demo{
       }
   }
 
+  public static SuperArray findOverlap(SuperArray a, SuperArray b){
+    removeDuplicates(a);
+    removeDuplicates(b);
+    SuperArray Intersection= new SuperArray();
+    for (int i=0; i<a.size();i++) {
+      for (int j=0; j<b.size();j++){
+        if (a.get(i).equals(b.get(j))){
+          Intersection.add (a.get(i));
+        }
+      }
+    }
+    return Intersection;
+  }
+
+  /*----
+  Tests
+  ----*/
   public static void main(String[]args){
 
     SuperArray words = new SuperArray();
@@ -23,5 +42,23 @@ public class Demo{
 
     System.out.println(words);
     removeDuplicates(words);
-    System.out.println(words);  }
+    System.out.println(words);
+
+    SuperArray A = new SuperArray();
+    A.add ("9");
+    A.add ("1");
+    A.add ("2");
+    A.add ("2");
+    A.add ("3");
+    A.add ("4");
+    SuperArray B = new SuperArray();
+    B.add ("0");
+    B.add ("4");
+    B.add ("2");
+    B.add ("2");
+    B.add ("9");
+
+    System.out.println(findOverlap(A, B));
+
+  }
 }
