9ba95d83e10d34a23f06486e94b410d0334c4745
vli21 <vli21@stuy.edu>
2020-11-09 16:29:37 -0500

    Everything works up to now

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ public class Demo{
 /*-------
 Methods
 -------*/
-  public static void removeDuplicates(SuperArray s){
+  public static void removeDuplicates(SuperArray s) {
       for (int i=0; i <s.size();){
         if (s.indexOf(s.get(i))==i){
           i++;
@@ -13,7 +13,7 @@ Methods
       }
   }
 
-  public static SuperArray findOverlap(SuperArray a, SuperArray b){
+  public static SuperArray findOverlap(SuperArray a, SuperArray b) {
     removeDuplicates(a);
     removeDuplicates(b);
     SuperArray Intersection= new SuperArray();
@@ -27,10 +27,42 @@ Methods
     return Intersection;
   }
 
+  public static SuperArray zip(SuperArray a, SuperArray b) {
+    int length= 0;
+    SuperArray mush= new SuperArray ();
+    if (a.size()>b.size()) {
+      length= a.size();
+    }
+    else {
+      length= b.size();
+    }
+    for (int i=0; i<length; i++){
+      if(length==a.size()){
+        if (i<b.size()){
+          mush.add (a.get(i));
+          mush.add (b.get(i));
+        }
+        else{
+          mush.add(a.get(i));
+        }
+      }
+      if(length==b.size()){
+        if (i<a.size()){
+          mush.add (a.get(i));
+          mush.add (b.get(i));
+        }
+        else{
+          mush.add(b.get(i));
+        }
+      }
+    }
+    return mush;
+  }
+
   /*----
   Tests
   ----*/
-  public static void main(String[]args){
+  public static void main(String[]args) {
 
     SuperArray words = new SuperArray();
     //grouped to save vertical space
@@ -59,6 +91,36 @@ Methods
     B.add ("9");
 
     System.out.println(findOverlap(A, B));
+
+    /* works
+    SuperArray one= new SuperArray();
+    one.add("a");
+    one.add("b");
+    one.add("c");
+    one.add("d");
+    one.add("e");
+    one.add("f");
+    SuperArray two= new SuperArray();
+    two.add("0");
+    two.add("1");
+    two.add("2");
+    two.add("3");
+
+    System.out.println(zip(one,two));
+    */
+    SuperArray one= new SuperArray();
+    one.add("a");
+    one.add("b");
+    one.add("c");
+
+    SuperArray two= new SuperArray();
+    two.add("0");
+    two.add("1");
+    two.add("2");
+    two.add("3");
+    two.add("4");
+
+    System.out.println(zip(one,two));
 
   }
 }
